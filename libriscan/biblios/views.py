2eff2f37d927263cdc071d4525081a6648d7f9a2
Michael Hampson <michael@flyingseamonsters.com>
2025-09-03 21:21:35 -0400

    HTMX and base templates

--- libriscan/biblios/views.py
+++ libriscan/biblios/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Insert Libriscan here.")
+    context = {"app_name": "Libriscan"}
+    return render(request, "biblios/index.html", context)
--- libriscan/libriscan/settings.py
+++ libriscan/libriscan/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'biblios',
+    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'libriscan.urls'
