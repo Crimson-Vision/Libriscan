d3d3e2bd8396d7dd15dd79560d009d3c310a0e98
Michael Hampson <michael@flyingseamonsters.com>
2025-09-07 09:17:02 -0400

    Org and consortium pages + views + urls

--- libriscan/biblios/views.py
+++ libriscan/biblios/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+from .models import Organization, Consortium
 
 def index(request):
-    context = {"app_name": "Libriscan"}
+    orgs = Organization.objects.all()
+    context = {
+        "app_name": "Libriscan",
+        "orgs":orgs
+    }
     return render(request, "biblios/index.html", context)
+
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = "orgs"
+
+
+class OrganizationDetail(DetailView):
+    model = Organization
+    context_object_name = "org"
+
+
+class ConsortiumDetail(DetailView):
+    model = Consortium
+    context_object_name = "consortium"
