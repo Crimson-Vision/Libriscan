44c525e636b7c3e516eaad6e90f4d41bbd4f2f0a
Michael Hampson <michael@flyingseamonsters.com>
2025-09-07 17:27:48 -0400

    Model admin, fixed membership fixture

--- libriscan/biblios/admin.py
+++ libriscan/biblios/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Organization, Consortium, Membership
+from .models import User, Organization, Consortium, Membership, Collection, Series, Document, Page, TextBlock
 
 
 
@@ -8,6 +8,13 @@ class MembershipInline(admin.TabularInline):
     model = Membership
     extra = 0
 
+class SeriesInline(admin.TabularInline):
+    model = Series
+    extra = 1
+
+class TextBlockInline(admin.StackedInline):
+    model = TextBlock
+    extra = 1
 
 @admin.register(Organization)
 class OrgAdmin(admin.ModelAdmin):
@@ -17,6 +24,21 @@ class OrgAdmin(admin.ModelAdmin):
 @admin.register(Consortium)
 class ConsortiumAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    inlines = [SeriesInline]
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    inlines = [TextBlockInline]
 
 
 admin.site.register(User, UserAdmin)
