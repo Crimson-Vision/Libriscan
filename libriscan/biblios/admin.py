70f19474216e8cafb9fd2b989d688e37368d58dd
Michael Hampson <michael@flyingseamonsters.com>
2025-09-07 08:45:38 -0400

    Core org and consortium models

--- libriscan/biblios/admin.py
+++ libriscan/biblios/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Organization, Consortium, Membership
+
+
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 0
+
+
+@admin.register(Organization)
+class OrgAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline]
+
+
+@admin.register(Consortium)
+class ConsortiumAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(User, UserAdmin)
+
