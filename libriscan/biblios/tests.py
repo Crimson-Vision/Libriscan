cf76b542b2aa331045ac19d14413a66f7acd60b4
Michael Hampson <michael@flyingseamonsters.com>
2025-09-11 22:48:21 -0400

    UserRole unique tests

--- libriscan/biblios/tests.py
+++ libriscan/biblios/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 
-from .models import Series, Document
+from .models import Series, Document, Organization, UserRole
 
 
 class UserModelTests(TestCase):
@@ -23,18 +23,38 @@ class BibliosTests(TestCase):
     # The unique constraint tests are confirming that the constraints are applied as intended,
     # not that they generally work.
 
-    def test_unique_doc_constraint(self):
+    def test_unique_docs(self):
+        # Checking that the constraint isn't being applied across different series
         d = Document.objects.first()
         s = Series.objects.create(collection=d.series.collection, name="Test series")
-        d2 = Document.objects.create(series=s)
+        
         try:
-            d2.identifier = d.identifier
-            d2.save()
+            Document.objects.create(series=s, identifier=d.identifier)
             passes = True
         except IntegrityError:
             passes = False
                 
         self.assertTrue(passes)
+
+
+    def test_unique_user_roles(self):
+        # Checking the constraint doesn't block a user from having different roles, 
+        # or different users from having the same role in an org
+        user_model = get_user_model()
+        u = user_model.objects.create(username="tester")
+        org = Organization.objects.first()
+        UserRole.objects.create(user=u, organization=org, role=UserRole.EDITOR)
+
+        try:
+            new_user = user_model.objects.create(username="tester2")
+            UserRole.objects.create(user=new_user, organization=org, role=UserRole.EDITOR)
+            UserRole.objects.create(user=u, organization=org, role=UserRole.ADMIN)
+            passes = False
+        except IntegrityError:
+            passes = True
+        
+        self.assertTrue(passes)
+
 
 
 
