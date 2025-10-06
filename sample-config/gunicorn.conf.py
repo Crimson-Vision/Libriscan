5227c4f2ddbde2acd0e0af53fd6e70abb8b1d5c7
Michael Hampson <michael@flyingseamonsters.com>
2025-10-05 20:55:56 -0400

    Move gunicorn config to conf file, with sample

---
+++ sample-config/gunicorn.conf.py
@@ -0,0 +1,26 @@
+import multiprocessing
+
+# Gunicorn Settings
+# See https://docs.gunicorn.org/en/stable/settings.html for full details and additional options
+
+# socket address to bind
+bind = "0.0.0.0:8000"
+
+# calculate number of workers
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# use threaded workers
+worker_class = 'gthread'
+
+# if this value is not 1, gthread workers will be used regardless of the worker_class
+threads = multiprocessing.cpu_count() * 2 if worker_class == 'gthread' else 1
+
+# worker timeout in seconds
+timeout = 120
+
+# restart a worker after a given number of requests. 
+# some jitter keeps them all from restarting at the same time
+max_requests = 500
+max_requests_jitter = 25
+
+
