3ebc2c22b8aafca7485ce4e662b62ea51f29e00f
Abdell Gautier <abdellgautier@hotmail.com>
2025-10-20 21:13:21 -0400

    Enforced DEBUG = False if no environmental variable is present.

--- libriscan/libriscan/settings.py
+++ libriscan/libriscan/settings.py
@@ -33,7 +33,8 @@ load_dotenv(dotenv_path=LOCAL_DIR / ".env")
 SECRET_KEY = os.environ.get("LB_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("LB_DEBUG", "True") == "True"
+# Default to DEBUG=False unless the LB_DEBUG environment variable is explicitly "True".
+DEBUG = os.environ.get("LB_DEBUG") == "True"
 
 ALLOWED_HOSTS = os.environ.get("LB_ALLOWED_HOSTS", "127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = os.environ.get("LB_TRUSTED_ORIGINS", "http://localhost").split(
@@ -44,7 +45,7 @@ SECURE_SSL_REDIRECT = not DEBUG
 
 # This prevents the app from crashing for local development
 # while ensuring a key is required for production
-if not SECRET_KEY and DEBUG:
+if DEBUG and not SECRET_KEY:
     SECRET_KEY = "django-insecure-change-me-for-a-real-project"
 
 # Application definition
