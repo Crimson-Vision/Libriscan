b5131f7357ffbd488cf7cb2367a0a857114b9821
Sein Tun <contact@seintun.dev>
2025-11-30 10:30:27 -0800

    Hide button of Add new collection and Delete Collection/Series/Document/Page for guest

---
+++ libriscan/biblios/templatetags/permissions.py
@@ -0,0 +1,48 @@
+from django import template
+from biblios.access_rules import is_org_viewer, is_org_editor, is_org_archivist
+from biblios.models import UserRole
+
+register = template.Library()
+
+
+@register.filter
+def can_view_org(user, org):
+    """Check if user can view organization."""
+    if not user.is_authenticated:
+        return False
+    return is_org_viewer(user, org)
+
+
+@register.filter
+def can_edit_org(user, org):
+    """Check if user can edit organization (Editor or Archivist, not Guest)."""
+    if not user.is_authenticated:
+        return False
+    return is_org_editor(user, org)
+
+
+@register.filter
+def can_admin_org(user, org):
+    """Check if user is archivist in organization."""
+    if not user.is_authenticated:
+        return False
+    return is_org_archivist(user, org)
+
+
+@register.filter
+def is_guest(user, org):
+    """Check if user is a guest in the organization."""
+    if not user.is_authenticated:
+        return True
+    return user.userrole_set.filter(
+        organization=org, role=UserRole.GUEST
+    ).exists()
+
+
+@register.filter
+def is_not_guest(user, org):
+    """Check if user is NOT a guest (i.e., Editor or Archivist)."""
+    if not user.is_authenticated:
+        return False
+    return is_org_editor(user, org)
+
